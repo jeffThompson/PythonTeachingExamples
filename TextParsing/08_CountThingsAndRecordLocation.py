be64a3a920e773ff80ba755c40002176a2224cb3
Jeff Thompson <mail@jeffreythompson.org>
2013-02-28 16:32:01 -0600

    Small semantic change for clarity

--- TextParsing/08_CountThingsAndRecordLocation.py
+++ TextParsing/08_CountThingsAndRecordLocation.py
@@ -30,12 +30,12 @@ for line in file:
 	fullText += line
 file.close()
 
-# grab page-length chunks (source: http://answers.yahoo.com/question/index?qid=20080814062544AAJ01qr)
+# split into page-length chunks (source: http://answers.yahoo.com/question/index?qid=20080814062544AAJ01qr)
 pageSize = 25 * 65 									# average 25 lines/page at 65 chars/line
 for start in range(0, len(fullText), pageSize):		# run through text from pos 0 - end in pageSize steps!
 	pages.append(fullText[start:start+pageSize])	# add an entry to the list of the specified size
 
-# open file and read line-by-line
+# open file and read page-by-page
 for page in pages:
 	instancesOfWord = re.findall("Frodo", page)		# find all instances of the word and return as a list
 	totalCount += len(instancesOfWord)				# add the length of the list (# of words matched) to our overall count
